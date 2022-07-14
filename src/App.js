7fd3e654bed3fce032d78e02176c795566b0f8d2
unknown <palakkotwani2883@gmail.com>
2022-07-14 23:36:50 +0530

    Added Tinder Cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Tinder Cards */}
-      {/* SwipeButtons */}
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
